d82c280b76fb05439a0e0ce78e56ecbf9e731291
jana47 <jana.prechelt@me.com>
2020-06-24 12:17:52 +0200

    added some style

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,12 @@ from app.models import Config
 
 
 class ConfigForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ConfigForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Config
         fields = '__all__'
