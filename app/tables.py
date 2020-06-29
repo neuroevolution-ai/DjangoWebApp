a59b5f3a46d3b7623457222d0b067f2f6aad3bfd
jana47 <jana.prechelt@me.com>
2020-06-29 10:31:53 +0200

    nobody likes shotgun surgeries

--- app/tables.py
+++ app/tables.py
@@ -16,24 +16,8 @@ class JobQueueTable(tables.Table):
         verbose_name=''
     )
 
-    # TODO: Felder nicht einzeln aufzählen!
     class Meta:
         model = Job
-        fields = ('environment',
-                  'neural_network_type',
-                  'random_seed',
-                  'number_generations',
-                  'trainer_type',
-                  'trainer_population_size',
-                  'trainer_sigma',
-                  'brain_number_neurons',
-                  'brain_delta_t',
-                  'brain_optimize_state_boundaries',
-                  'brain_clipping_range_max',
-                  'brain_clipping_range_min',
-                  'brain_optimize_y0',
-                  'brain_set_principle_diagonal_elements_of_W_negative',
-                  'episode_runner_keep_env_seed_fixed_during_generation',
-                  'delete')
+        exclude = ('id', )
         template_name = "django_tables2/bootstrap.html"
         orderable = False
