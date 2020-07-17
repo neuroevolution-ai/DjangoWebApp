b8d99d05d9214e6fcd8b60ee86a3255c1e2a61bd
jana47 <jana.prechelt@me.com>
2020-07-17 14:34:15 +0200

    filljobqueue angepasst an neue felder

--- app/management/commands/filljobqueue.py
+++ app/management/commands/filljobqueue.py
@@ -7,7 +7,6 @@ import logging
 logger = logging.getLogger('root')
 
 
-# TODO: Anzahl Minuten und Anzahl Jobs konfigurierbar machen
 class Command(BaseCommand):
     help = 'fills jobqueue every 10 seconds if number of jobs < 5'
 
@@ -35,22 +34,46 @@ class Command(BaseCommand):
     def __create_job_from_config(self, config):
         job = Job()
         job.number = self.__get_number_of_last_job() + 1
+
         job.environment = choice(config.environment)
-        job.neural_network_type = choice(config.neural_network_type)
         job.random_seed = choice(config.random_seed)
         job.number_generations = choice(config.number_generations)
-        job.trainer_type = choice(config.trainer_type)
-        job.trainer_population_size = choice(config.trainer_population_size)
-        job.trainer_sigma = float(choice(config.trainer_sigma))
+        job.use_worker_processes = config.use_worker_processes
+
+        job.optimizer_type = choice(config.optimizer_type)
+        job.optimizer_population_size = choice(config.optimizer_population_size)
+        job.optimizer_sigma = choice(config.optimizer_sigma)
+        job.optimizer_checkpoint_frequency = choice(config.optimizer_checkpoint_frequency)
+        job.optimizer_hof_size = choice(config.optimizer_hof_size)
+
+        job.brain_type = choice(config.brain_type)
         job.brain_number_neurons = choice(config.brain_number_neurons)
-        job.brain_delta_t = float(choice(config.brain_delta_t))
-        job.brain_optimize_state_boundaries = config.brain_optimize_state_boundaries
-        job.brain_clipping_range_max = float(choice(config.brain_clipping_range_max))
-        job.brain_clipping_range_min = float(choice(config.brain_clipping_range_min))
+        job.brain_use_bias = config.brain_use_bias
+        job.brain_delta_t = choice(config.brain_delta_t)
+        job.brain_neuron_activation = choice(config.brain_neuron_activation)
+        job.brain_neuron_activation_inplace = config.brain_neuron_activation_inplace
+        job.brain_normalize_input = config.brain_normalize_input
+        job.brain_normalize_input_target = choice(config.brain_normalize_input_target)
+        job.brain_optimize_state_boundaries = choice(config.brain_optimize_state_boundaries)
+        job.brain_clipping_range_max = choice(config.brain_clipping_range_max)
+        job.brain_clipping_range_min = choice(config.brain_clipping_range_min)
         job.brain_optimize_y0 = config.brain_optimize_y0
         job.brain_set_principle_diagonal_elements_of_W_negative = config.brain_set_principle_diagonal_elements_of_W_negative
+        job.brain_w_mask = choice(config.brain_w_mask)
+        job.brain_w_mask_param = choice(config.brain_w_mask_param)
+        job.brain_v_mask = choice(config.brain_v_mask)
+        job.brain_v_mask_param = choice(config.brain_v_mask_param)
+        job.brain_t_mask = choice(config.brain_t_mask)
+        job.brain_t_mask_param = choice(config.brain_t_mask_param)
+        job.brain_parameter_perturbations = choice(config.brain_parameter_perturbations)
+
+        job.episode_runner_type = choice(config.episode_runner_type)
         job.episode_runner_keep_env_seed_fixed_during_generation = config.episode_runner_keep_env_seed_fixed_during_generation
         job.episode_runner_number_fitness_runs = choice(config.episode_runner_number_fitness_runs)
+        job.episode_runner_reuse_env = config.episode_runner_reuse_env
+        job.episode_runner_max_steps_per_run = choice(config.episode_runner_max_steps_per_run)
+        job.episode_runner_max_steps_penalty = choice(config.episode_runner_max_steps_penalty)
+
         return job
 
     @staticmethod
