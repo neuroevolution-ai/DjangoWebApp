9257f617ff5095723ad19c91c0e3de7fa06ee911
jana47 <jana.prechelt@me.com>
2020-07-17 18:19:50 +0200

    updated create_json_from_job

--- app/management/commands/executejobs.py
+++ app/management/commands/executejobs.py
@@ -6,7 +6,7 @@ import json
 logger = logging.getLogger('training')
 
 
-# TODO: updated README.md
+# TODO: update README.md
 class Command(BaseCommand):
     help = 'executes jobs from job queue'
 
@@ -29,48 +29,49 @@ class Command(BaseCommand):
 
     @staticmethod
     def __create_json_from_job(job):
-        configuration = job.__dict__
-
-        # convert decimal to float
-        configuration['trainer_sigma'] = float(configuration['trainer_sigma'])
-        configuration['brain_delta_t'] = float(configuration['brain_delta_t'])
-        configuration['brain_clipping_range_max'] = float(configuration['brain_clipping_range_max'])
-        configuration['brain_clipping_range_min'] = float(configuration['brain_clipping_range_min'])
-
-        # nest
-        configuration['trainer'] = {
-            'population_size': configuration['trainer_population_size'],
-            'sigma': configuration['trainer_sigma']
+        configuration = dict()
+        configuration['environment'] = job.environment
+        configuration['random_seed'] = job.random_seed
+        configuration['number_generations'] = job.number_generations
+        configuration['use_worker_processes'] = job.use_worker_processes
+        configuration['optimizer'] = {
+            'type': job.optimizer_type,
+            'population_size': job.optimizer_population_size,
+            'sigma': float(job.optimizer_sigma),
+            'checkpoint_frequency': job.optimizer_checkpoint_frequency,
+            'hof_size': job.optimizer_hof_size
         }
         configuration['brain'] = {
-            'number_neurons': configuration['brain_number_neurons'],
-            'delta_t': configuration['brain_delta_t'],
-            'optimize_state_boundaries': configuration['brain_optimize_state_boundaries'],
-            'clipping_range_max': configuration['brain_clipping_range_max'],
-            'clipping_range_min': configuration['brain_clipping_range_min'],
-            'optimize_y0': configuration['brain_optimize_y0'],
-            'set_principle_diagonal_elements_of_W_negative': configuration['brain_set_principle_diagonal_elements_of_W_negative']
-        }
-        configuration['episode_runner'] = {
-            'keep_env_seed_fixed_during_generation': configuration['episode_runner_keep_env_seed_fixed_during_generation'],
-            'number_fitness_runs': configuration['episode_runner_number_fitness_runs']
+            'type': job.brain_type,
+            'number_neurons': job.brain_number_neurons,
+            'use_bias': job.brain_use_bias,
+            'delta_t': float(job.brain_delta_t),
+            'neuron_activation': job.brain_neuron_activation,
+            'neuron_activation_inplace': job.brain_neuron_activation_inplace,
+            'normalize_input': job.brain_normalize_input,
+            'normalize_input_target': job.brain_normalize_input_target,
+            'optimize_state_boundaries': job.brain_optimize_state_boundaries,
+            'clipping_range_max': float(job.brain_clipping_range_max),
+            'clipping_range_min': float(job.brain_clipping_range_min),
+            'optimize_y0': job.brain_optimize_y0,
+            'set_principle_diagonal_elements_of_W_negative': job.brain_set_principle_diagonal_elements_of_W_negative,
+            'w_mask': job.brain_w_mask,
+            'w_mask_param': job.brain_w_mask_param,
+            'v_mask': job.brain_v_mask,
+            'v_mask_param': job.brain_v_mask_param,
+            't_mask': job.brain_t_mask,
+            't_mask_param': job.brain_t_mask_param,
+            'parameter_perturbations': float(job.brain_parameter_perturbations)
         }
 
-        # delete unused keys
-        configuration.pop('_state')
-        configuration.pop('id')
-        configuration.pop('number')
-        configuration.pop('trainer_sigma')
-        configuration.pop('trainer_population_size')
-        configuration.pop('brain_number_neurons')
-        configuration.pop('brain_delta_t')
-        configuration.pop('brain_optimize_state_boundaries')
-        configuration.pop('brain_clipping_range_max')
-        configuration.pop('brain_clipping_range_min')
-        configuration.pop('brain_optimize_y0')
-        configuration.pop('brain_set_principle_diagonal_elements_of_W_negative')
-        configuration.pop('episode_runner_keep_env_seed_fixed_during_generation')
-        configuration.pop('episode_runner_number_fitness_runs')
+        configuration['episode_runner'] = {
+            'type': job.episode_runner_type,
+            'keep_env_seed_fixed_during_generation': job.episode_runner_keep_env_seed_fixed_during_generation,
+            'number_fitness_runs': job.episode_runner_number_fitness_runs,
+            'reuse_env': job.episode_runner_reuse_env,
+            'max_steps_per_run': job.episode_runner_max_steps_per_run,
+            'max_steps_penalty': job.episode_runner_max_steps_penalty
+        }
 
         with open('Configuration.json', 'w') as file:
             json.dump(configuration, file)
