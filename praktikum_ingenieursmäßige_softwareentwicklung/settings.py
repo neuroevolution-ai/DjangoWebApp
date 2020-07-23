e71968e784e718888fa11dc964520e567e65662c
jap <jana.prechelt@me.com>
2020-07-23 14:44:27 +0200

    remove sk

--- praktikum_ingenieursmäßige_softwareentwicklung/settings.py
+++ praktikum_ingenieursmäßige_softwareentwicklung/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nn0=qnnyr52ilfip)8ap!p%o5!rq@h26ib6ai(k-6@%%fo#08m'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
